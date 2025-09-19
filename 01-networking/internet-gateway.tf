b2ef0f74f6da4c3704ce7f80c0233a6b3dc96fee
Alves0611 <g.alves0611@gmail.com>
2025-09-19 20:35:10 -0300

    refactor: remove old internet gateway file

--- 01-networking/internet-gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags       = merge({ Name = "${var.vpc_resources.name}-${var.vpc_resources.internet_gateway}" }, var.tags)
-  depends_on = [aws_vpc.this]
-}
