1f1682111023e1e980ad6f59ec0dd6440ec12ef3
Alves0611 <g.alves0611@gmail.com>
2025-09-19 20:07:59 -0300

    feat: extend VPC variables to support public subnets and route table configuration

--- 01-networking/variables.tf
+++ 01-networking/variables.tf
@@ -6,14 +6,34 @@ variable "region" {
 
 variable "vpc_resources" {
   type = object({
-    cidr_block = string
-    name       = string
-    internet_gateway = string
+    cidr_block              = string
+    name                    = string
+    internet_gateway        = string
+    public_route_table_name = string
+    public_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
   })
   default = {
-    cidr_block = "10.0.0.0/24"
-    name       = "eks-vpc"
-    internet_gateway = "igw"
+    cidr_block              = "10.0.0.0/24"
+    name                    = "eks-vpc"
+    internet_gateway        = "igw"
+    public_route_table_name = "public-route-table"
+    public_subnets = [{
+      name                    = "public-subnet-1a"
+      cidr_block              = "10.0.0.0/27"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = true
+      },
+      {
+        name                    = "public-subnet-1b"
+        cidr_block              = "10.0.0.64/27"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = true
+    }]
   }
 }
 
