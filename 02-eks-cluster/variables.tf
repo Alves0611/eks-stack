d0bcf4b769ea1fdf501f37502e143ca90af5be5e
Alves0611 <g.alves0611@gmail.com>
2025-09-19 23:38:48 -0300

    feat: add node group variables

--- 02-eks-cluster/variables.tf
+++ 02-eks-cluster/variables.tf
@@ -10,9 +10,20 @@ variable "eks_cluster" {
     role_name                 = string
     version                   = string
     enabled_cluster_log_types = list(string)
-
     access_config = object({
       authentication_mode = string
+    })
+    node_group = object({
+      name           = string
+      role_name      = string
+      instance_types = list(string)
+      capacity_type  = string
+      ami_type       = string
+      scaling_config = object({
+        desired_size = number
+        max_size     = number
+        min_size     = number
+      })
     })
   })
   default = {
@@ -22,6 +33,18 @@ variable "eks_cluster" {
     enabled_cluster_log_types = ["audit", "api", "authenticator", "controllerManager", "scheduler"]
     access_config = {
       authentication_mode = "API_AND_CONFIG_MAP"
+    }
+    node_group = {
+      name           = "studying-node-group"
+      role_name      = "studying-node-group-role"
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+      ami_type       = "AL2023_x86_64_STANDARD"
+      scaling_config = {
+        desired_size = 1
+        max_size     = 3
+        min_size     = 1
+      }
     }
   }
 }
