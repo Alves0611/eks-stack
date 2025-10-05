2b4e795093de047480443ce94075a0c6d3ca751b
Alves0611 <g.alves0611@gmail.com>
2025-10-05 03:36:24 -0300

    chore: add outputs for observability subnet IDs and CIDR blocks

--- 01-networking/outputs.tf
+++ 01-networking/outputs.tf
@@ -5,3 +5,13 @@ output "vpc_id" {
 output "internet_gateway_id" {
   value = aws_internet_gateway.this.id
 }
+
+output "observability_subnet_ids" {
+  description = "IDs of the observability subnets"
+  value       = aws_subnet.observability[*].id
+}
+
+output "observability_subnet_cidrs" {
+  description = "CIDR blocks of the observability subnets"
+  value       = aws_subnet.observability[*].cidr_block
+}
