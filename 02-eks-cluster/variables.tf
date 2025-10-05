62a9cc328f47a0c8e6bef877dfb7386c56e5dd06
Alves0611 <g.alves0611@gmail.com>
2025-10-05 03:02:31 -0300

    chore: update variable descriptions to remove S3 bucket reference

--- 02-eks-cluster/variables.tf
+++ 02-eks-cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   type        = string
-  description = "The region to deploy the S3 bucket"
+  description = "The region to deploy"
   default     = "us-east-1"
 }
 
@@ -51,7 +51,7 @@ variable "eks_cluster" {
 
 variable "tags" {
   type        = map(string)
-  description = "The tags to apply to the S3 bucket"
+  description = "The tags to apply"
   default = {
     ManagedBy = "Terraform"
     Project   = "eks-stack"
