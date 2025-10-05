1b536590b23f8b1be766eba663fc391b4d192fa2
Alves0611 <g.alves0611@gmail.com>
2025-10-05 03:44:18 -0300

    chore: add output to expose EKS cluster ARN

--- 02-eks-cluster/outputs.tf
+++ 02-eks-cluster/outputs.tf
@@ -3,6 +3,11 @@ output "eks_cluster_name" {
   value       = aws_eks_cluster.this.name
 }
 
+output "eks_cluster_arn" {
+  description = "ARN of the EKS cluster"
+  value       = aws_eks_cluster.this.arn
+}
+
 output "eks_cluster_certificate_authority_data" {
   description = "Certificate authority data of the EKS cluster"
   value       = aws_eks_cluster.this.certificate_authority[0].data
